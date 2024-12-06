ed397f9a98616dbc514fea0627886990daee8703
Kevin Truong <kevintruong6587@gmail.com>
2024-12-06 12:24:03 +0700

    ???

--- setup.sh
+++ setup.sh
@@ -3,16 +3,23 @@
 # Yêu cầu người dùng nhập các giá trị môi trường
 echo "Creating .env file..."
 
-echo "Enter your API_KEY:"
-read API_KEY
 echo "Enter your DB_PASSWORD:"
 read DB_PASSWORD
-echo "Enter your SECRET_KEY:"
-read SECRET_KEY
+echo "Enter your DB_HOST (e.g., moixemdoannay.site):"
+read DB_HOST
+echo "Enter your DB_NAME (e.g., concac_db):"
+read DB_NAME
 
-# Tạo file .env với các giá trị nhập vào
+# Tạo URL PostgreSQL từ các giá trị đã nhập
+DB_USER="postgres"
+DB_PORT="5432"  # Sử dụng cổng mặc định của PostgreSQL
+
+DATABASE_POSTGRES_URL="postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME"
+
+# Tạo file .env với các giá trị nhập vào và URL PostgreSQL
 echo "API_KEY=$API_KEY" > .env
 echo "DB_PASSWORD=$DB_PASSWORD" >> .env
 echo "SECRET_KEY=$SECRET_KEY" >> .env
+echo "DATABASE_POSTGRES_URL=$DATABASE_POSTGRES_URL" >> .env
 
 echo ".env file has been created successfully!"
